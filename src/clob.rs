86ab8cb439448ef7f2148f5c791635f5bf88be51
Chaz Byrnes <chaz@polymarket.com>
2025-12-09 09:45:52 -0500

    chore: various cleanup

--- src/clob.rs
+++ src/clob.rs
@@ -286,6 +286,13 @@ pub struct Client<S: State = Unauthenticated> {
     inner: Arc<ClientInner<S>>,
 }
 
+impl Default for Client<Unauthenticated> {
+    fn default() -> Self {
+        Client::new("https://clob.polymarket.com", Config::default())
+            .expect("Client with default endpoint should succeed")
+    }
+}
+
 /// Configuration for [`Client`]
 #[derive(Clone, Debug, Default, Builder)]
 #[builder(pattern = "owned", build_fn(error = "Error"))]
