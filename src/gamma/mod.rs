dcf5dc49838d4d93106bae7fe40f7c6ab7b08cfb
chaz-polymarket <chaz@polymarket.com>
2025-12-29 18:55:46 -0500

    refactor!: rescope gamma types (#61)
    No functional changes -- just moving types

--- src/gamma/mod.rs
+++ src/gamma/mod.rs
@@ -39,7 +39,7 @@
 //! # Example
 //!
 //! ```no_run
-//! use polymarket_client_sdk::gamma::{Client, types::EventsRequest};
+//! use polymarket_client_sdk::gamma::{Client, types::request::EventsRequest};
 //!
 //! # async fn example() -> Result<(), Box<dyn std::error::Error>> {
 //! // Create a client with the default endpoint
